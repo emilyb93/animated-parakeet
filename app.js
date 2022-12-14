053361d6cce36d4dd8aa8d496f82ec1ee526c477
Emily Bennett <emily.bennett@northcoders.com>
2022-12-14 11:53:16 +0000

    enabling cors

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const db = require("./db/connection");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 app.get("/api/lesson", (req, res) => {
   db.query("SELECT * FROM lessons_content").then(({ rows }) => {
